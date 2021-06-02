d7b70dd5b6f6606f4e5fe1bd3556ee0090ba0945
Hoàng Minh <58630186+Hngdcmnh@users.noreply.github.com>
2021-06-02 15:38:35 +0700

    update2

--- app/src/main/java/com/example/learn_android/MainActivity.java
+++ app/src/main/java/com/example/learn_android/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.learn_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.button);
+
     }
 }
---
+++ app/src/main/java/com/example/learn_android/New1.java
@@ -0,0 +1,4 @@
+package com.example.learn_android;
+
+public class New1 {
+}
